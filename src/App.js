ecaf0734d8d6fc9c6d924ef8fe30b8a1f8e8fb01
ShivasaiprasadAkula <120090688+ShivasaiprasadAkula@users.noreply.github.com>
2024-10-28 20:33:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
 import MainPage from "./components/MainPage";
 
 function App() {
